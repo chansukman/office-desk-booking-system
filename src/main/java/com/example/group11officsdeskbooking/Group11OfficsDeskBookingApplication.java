646a7112849d8cf04ed4626f221849975977e4d3
Dan Harling <harlingd@cardiff.ac.uk>
2021-11-16 14:06:08 +0000

    initial commit

---
+++ src/main/java/com/example/group11officsdeskbooking/Group11OfficsDeskBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.group11officsdeskbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group11OfficsDeskBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group11OfficsDeskBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/group11officsdeskbooking/Group11OfficsDeskBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.group11officsdeskbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group11OfficsDeskBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
