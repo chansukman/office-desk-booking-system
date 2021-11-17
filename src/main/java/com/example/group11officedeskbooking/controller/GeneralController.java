3fed96164c2f2a105e418003a5a56748e9426e70
Dan Harling <harlingd@cardiff.ac.uk>
2021-11-17 18:45:54 +0000

    add calendar date search (html & route)

--- src/main/java/com/example/group11officedeskbooking/controller/GeneralController.java
+++ src/main/java/com/example/group11officedeskbooking/controller/GeneralController.java
@@ -1,8 +1,14 @@
 package com.example.group11officedeskbooking.controller;
 
+import org.apache.tomcat.util.digester.ArrayStack;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class GeneralController {
@@ -19,6 +25,21 @@ public class GeneralController {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("bookings");
         return mav;
+    }
+
+    @RequestMapping(path = "/searchDate", method = RequestMethod.GET)
+    public ModelAndView search(@RequestParam(value="date") String date){
+        ModelAndView mav = new ModelAndView();
+        String[] testDesk = new String[5];
+        testDesk[0] = "Desk 1";
+        testDesk[1] = "Standard Desk";
+        testDesk[2] = "Occupied";
+        testDesk[3] = "image";
+        testDesk[4] = date;
+        mav.addObject("testDesk", testDesk);
+        mav.setViewName("bookings");
+        return mav;
+
     }
 
 }
