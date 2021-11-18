24ef04a6a643d9c95b1e217b8cbfa3165651bd9a
c21086851 <chens100@cardiff.ac.uk>
2021-11-18 13:06:02 +0000

    add Usercontroller

---
+++ src/main/java/com/example/group11officedeskbooking/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.example.group11officedeskbooking.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class UserController {
+
+}
