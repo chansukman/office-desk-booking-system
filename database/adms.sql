9b22368a1bf17a4b0db3dae5ef2434f6c38ed52f
Dan Harling <harlingd@cardiff.ac.uk>
2021-11-22 12:25:57 +0000

    revert alterations to adms.sql

--- database/adms.sql
+++ database/adms.sql
@@ -1,17 +1,47 @@
-CREATE DATABASE  IF NOT EXISTS `adms2` 
-USE `adms2`;
+CREATE DATABASE  IF NOT EXISTS `adms` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `adms`;
+-- MySQL dump 10.13  Distrib 8.0.22, for macos10.15 (x86_64)
+--
+-- Host: 127.0.0.1    Database: adms
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Booking`
+--
 
 DROP TABLE IF EXISTS `Booking`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Booking` (
-  `booking_id` int NOT NULL AUTO_INCREMENT,
-  `booking_date` date NOT NULL,
-  `booking_time` time NOT NULL,
-  `User_user_id` int NOT NULL,
-  `Desk_desk_id` int NOT NULL,
-  `transaction_timestamp` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`booking_id`,`User_user_id`,`Desk_desk_id`),
+                           `booking_id` int NOT NULL AUTO_INCREMENT,
+                           `booking_date` date NOT NULL,
+                           `booking_time` time NOT NULL,
+                           `User_user_id` int NOT NULL,
+                           `Desk_desk_id` int NOT NULL,
+                           `transaction_timestamp` timestamp NULL DEFAULT NULL,
+                           PRIMARY KEY (`booking_id`,`User_user_id`,`Desk_desk_id`),
+                           KEY `fk_Booking_User1_idx` (`User_user_id`),
+                           KEY `fk_Booking_Desk1_idx` (`Desk_desk_id`),
+                           CONSTRAINT `fk_Booking_Desk1` FOREIGN KEY (`Desk_desk_id`) REFERENCES `Desk` (`desk_id`),
+                           CONSTRAINT `fk_Booking_User1` FOREIGN KEY (`User_user_id`) REFERENCES `User` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `Booking`
+--
 
 LOCK TABLES `Booking` WRITE;
 /*!40000 ALTER TABLE `Booking` DISABLE KEYS */;
@@ -27,12 +57,12 @@ DROP TABLE IF EXISTS `Desk`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Desk` (
-  `desk_id` int NOT NULL,
-  `has_window` tinyint NOT NULL,
-  `desk_location` int DEFAULT NULL,
-  PRIMARY KEY (`desk_id`),
-  KEY `location_id_idx` (`desk_location`),
-  CONSTRAINT `location_id` FOREIGN KEY (`desk_location`) REFERENCES `location` (`location_id`)
+                        `desk_id` int NOT NULL,
+                        `has_window` tinyint NOT NULL,
+                        `desk_location` int DEFAULT NULL,
+                        PRIMARY KEY (`desk_id`),
+                        KEY `location_id_idx` (`desk_location`),
+                        CONSTRAINT `location_id` FOREIGN KEY (`desk_location`) REFERENCES `location` (`location_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -54,10 +84,10 @@ DROP TABLE IF EXISTS `location`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `location` (
-  `location_id` int NOT NULL AUTO_INCREMENT,
-  `city` varchar(50) NOT NULL,
-  `building_name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`location_id`)
+                            `location_id` int NOT NULL AUTO_INCREMENT,
+                            `city` varchar(50) NOT NULL,
+                            `building_name` varchar(45) DEFAULT NULL,
+                            PRIMARY KEY (`location_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -79,12 +109,12 @@ DROP TABLE IF EXISTS `User`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `User` (
-  `user_id` int NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `department` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
+                        `user_id` int NOT NULL,
+                        `first_name` varchar(50) NOT NULL,
+                        `last_name` varchar(50) NOT NULL,
+                        `email` varchar(50) NOT NULL,
+                        `department` varchar(50) DEFAULT NULL,
+                        PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
