1c41b7f9981c0e12be7ee0bfdcda242de20f0b36
Dan Harling <harlingd@cardiff.ac.uk>
2021-11-30 13:39:34 +0000

    add date to available desk cards

--- src/main/java/com/example/group11officedeskbooking/controller/DeskController.java
+++ src/main/java/com/example/group11officedeskbooking/controller/DeskController.java
@@ -1,13 +1,18 @@
 package com.example.group11officedeskbooking.controller;
 
+import com.example.group11officedeskbooking.DTO.DeskDTO;
+import com.example.group11officedeskbooking.DTO.MapDTO;
 import com.example.group11officedeskbooking.repository.DeskRepository;
 import com.example.group11officedeskbooking.repository.MapRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class DeskController {
@@ -32,6 +37,7 @@ public class DeskController {
         //Add map and desk to mav
         mav.addObject("map", mapRepo.searchMap(deskLocation));
         mav.addObject("deskList", deskRepo.searchAvailableDesksByDate(searchDate, deskLocation));
+        mav.addObject("searchDate", searchDate);
         mav.setViewName("bookings");
         return mav;
     }
