009d30074de3886117b8e8d3649a70aa2de9d6e1
c21112132 <alotaibia5@cardiff.ac.uk>
2021-12-06 13:53:21 +0000

    query updated to list only upcoming  user booking instead of list all records and date formatted for better readability.

--- src/main/java/com/example/group11officedeskbooking/repository/UserBookingRepositoryJDBC.java
+++ src/main/java/com/example/group11officedeskbooking/repository/UserBookingRepositoryJDBC.java
@@ -24,7 +24,7 @@ public class UserBookingRepositoryJDBC implements UserBookingRepository{
         // JdbcTemplate query used to get multiple records from the database
 
         return jdbcTemplate.query(
-            "SELECT booking_id,booking_date,desk_location,Desk_desk_id FROM Booking JOIN Desk ON Booking.Desk_desk_id = Desk.desk_id WHERE Booking.User_user_id=?",
+            "Select Booking_id,Booking_date, DATE_FORMAT(Booking_date, '%D %M %Y') AS formatedDate from Booking WHERE User_user_id =? && Booking_date >= now()",
 
                 new UserBookingMapper(), new Object[]{id});
 
