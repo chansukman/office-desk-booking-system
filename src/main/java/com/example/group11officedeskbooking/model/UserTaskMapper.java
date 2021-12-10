b0dd976979a30ee46e36e1b75fe37adcd615c473
c21112132 <alotaibia5@cardiff.ac.uk>
2021-12-10 18:26:10 +0000

    Mapper created

--- src/main/java/com/example/group11officedeskbooking/model/UserTaskMapper.java
+++ src/main/java/com/example/group11officedeskbooking/model/UserTaskMapper.java
@@ -1,2 +1,19 @@
-package com.example.group11officedeskbooking.model;public class UserTaskMapper {
+package com.example.group11officedeskbooking.model;
+
+import com.example.group11officedeskbooking.DTO.UserTask;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserTaskMapper implements RowMapper {
+    @Override
+    public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+        int id = rs.getInt("booking_id");
+        String date = rs.getString("booking_date");
+        int deskId = rs.getInt("Desk_desk_id");
+        int userId = rs.getInt("User_user_id");
+        UserTask userTask = new UserTask(id, date, deskId, userId);
+        return userTask;
+    }
 }
