561f3178e20fddc5cc3b472daeaa13810d1dd62d
c21112132 <alotaibia5@cardiff.ac.uk>
2021-12-10 18:33:36 +0000

    Repository created

--- src/main/java/com/example/group11officedeskbooking/repository/UserTaskRepository.java
+++ src/main/java/com/example/group11officedeskbooking/repository/UserTaskRepository.java
@@ -1,2 +1,29 @@
-package com.example.group11officedeskbooking.repository;public class UserTaskRepository {
+package com.example.group11officedeskbooking.repository;
+
+import com.example.group11officedeskbooking.DTO.UserTaskDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserTaskRepository implements UserTaskDAO {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    @Override
+    public int updateCancelTable(int id) {
+        return jdbcTemplate.update("INSERT INTO Cancel (booking_id, booking_date,User_user_id,Desk_desk_id)" +
+                "SELECT * FROM booking WHERE booking_id =?", new Object[]{id});
+        /* jdbcTemplate.update("DELETE FROM Booking where booking_id =?", new Object[]{id});*/
+    }
+
+
+    @Override
+    public int deleteBooking(int id) {
+        return jdbcTemplate.update("DELETE FROM Booking where booking_id =?", new Object[]{id});
+    }
 }
