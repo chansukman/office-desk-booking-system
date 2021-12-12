785278897cad5f6cdd16fbbf977a0b889d3e1522
c21086851 <chens100@cardiff.ac.uk>
2021-12-12 17:25:29 +0000

    change some comment of code

--- src/main/java/com/example/group11officedeskbooking/controller/AutoController.java
+++ src/main/java/com/example/group11officedeskbooking/controller/AutoController.java
@@ -37,28 +37,27 @@ public class AutoController {
     @RequestMapping("/callback")
     @ResponseBody
     public String callback(HttpServletResponse response2, @RequestParam("code") String code) throws IOException {
-        //1.code参数为github回调callback_uri时，github传递过来的
-        System.out.println("请求callback...,code:" + code);
+        //1.code form callback_uri
         Map<String, Object> map = new HashMap<>();
         map.put("client_id", client_id);
         map.put("client_secret", client_secret);
         map.put("code", code);
         map.put("redirect_url", "http://localhost:8080/callback");
         map.put("state", "test");
-        //获取access token
+        //access token
         String url = "https://github.com/login/oauth/access_token";
         String json = JSON.toJSONString(map);
-        //2.根据传入的参数（包含code），post请求https://github.com/login/oauth/access_token，获取返回值
+        //2.accoding to code,post https://github.com/login/oauth/access_token，
         String result = httpHelper.Post(url, json);//access_token=your_client_id&scope=user&token_type=bearer
         System.out.println("callback result:" + result);
 
         String[] strs = result.split("&");
-        String access_token = strs[0].split("=")[1];//解析access_token
+        String access_token = strs[0].split("=")[1];//access_token
 
-        //3.根据access token,请求https://api.github.com/user获取用户信息
+        //3.according to access token,https://api.github.com/user get user information
 //            String url_user = "https://api.github.com/user?access_token=" + access_token;
         String userInfo = httpHelper.Get(access_token);
-//            System.out.println("userInfo:" + userInfo);//返回的是一个json字符串
+//            System.out.println("userInfo:" + userInfo);
         GithubUser githubUser = JSON.parseObject(userInfo, GithubUser.class);
         Integer gitUserId = githubUser.getId();
         String gitUserName = githubUser.getLogin();
