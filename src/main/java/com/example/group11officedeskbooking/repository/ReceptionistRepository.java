8f7c2d124a55c2bb0e84cc088a119a5461b2ff56
c21112132 <alotaibia5@cardiff.ac.uk>
2021-12-14 18:22:08 +0000

    isAttended added to mapper

--- src/main/java/com/example/group11officedeskbooking/repository/ReceptionistRepository.java
+++ src/main/java/com/example/group11officedeskbooking/repository/ReceptionistRepository.java
@@ -30,7 +30,7 @@ public class ReceptionistRepository implements UserTaskDAO {
     @Override
     public List<ReceptionistTask> getDailyBooking(String deskLocation) {
         return  jdbcTemplate.query(
-                "SELECT booking_id,User_user_id,User.first_name, User.last_name, desk_location,Desk.desk_number,curdate()AS today  FROM Booking " +
+                "SELECT booking_id,User_user_id,User.first_name, User.last_name, desk_location,isAttended,Desk.desk_number,curdate()AS today  FROM Booking " +
                         "JOIN Desk ON Booking.Desk_desk_id = Desk.desk_id " +
                         "JOIN User ON Booking.User_user_id = User.user_id " +
                         "WHERE  Booking_date = CURDATE()&& desk_location=? ORDER BY booking_id",
